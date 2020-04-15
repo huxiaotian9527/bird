12c2a0f538e0ef6306f495143d7d4433b04281ea
hutiantian <hutiantian9527@gmail.com>
2020-04-15 17:39:14 +0800

    mybatis-plus整合

---
+++ src/main/java/com/hu/boot/bird/common/MpGenerator.java
@@ -0,0 +1,183 @@
+package com.hu.boot.bird.common;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @Author hutiantian
+ * @Date 2020/4/15 16:53:02
+ */
+
+public class MpGenerator {
+
+    private static String packageName = "";
+    //表名，逗号分隔
+    private static String tableName = "t_borrower_info,t_debt,t_debt_batch,t_debt_loan_detail,t_debt_original,t_debt_payback_detail,t_loan_business_info,t_loan_rate_info";
+
+
+    private static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入" + tip + "：");
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        final String projectPath = System.getProperty("user.dir");
+        System.out.println("projectPath:"+projectPath);
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("CJJ");
+        gc.setOpen(false);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        gc.setFileOverride(true);
+        // 不需要ActiveRecord特性的请改为false
+        gc.setActiveRecord(false);
+        // XML 二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(false);
+        // XML columList
+        gc.setBaseColumnList(false);
+        // .setKotlin(true) 是否生成 kotlin 代码
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setMapperName("%sMapper");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setEntityName("%sPO");
+        gc.setXmlName("%sMapper");
+        gc.setIdType(IdType.AUTO);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://172.22.23.160:3306/finance_shcis_credit?zeroDateTimeBehavior=convertToNull&useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true");
+        //dsc.setUrl("jdbc:mysql://localhost:3306/my_first?zeroDateTimeBehavior=convertToNull&useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true&serverTimezone=Asia/Shanghai");
+        /// dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        //dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        /*dsc.setUsername("JR_dev");
+        dsc.setPassword("dev160@sscf");*/
+        dsc.setUsername("JR_dev");
+        dsc.setPassword("dev160@sscf");
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        final PackageConfig pc = new PackageConfig();
+        pc.setModuleName(packageName);
+        pc.setParent("com.hu.boot.bird");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.vm";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mybatis/" //+ pc.getModuleName()
+                        + "/" + tableInfo.getEntityName().replace("PO","") + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        /*
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir("调用默认方法创建的目录");
+                return false;
+            }
+        });
+        */
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        //templateConfig.setEntity("templates/entity2.java");
+        //templateConfig.setService();
+        //templateConfig.setController();
+
+        templateConfig.setXml(null);
+        //选择要生成的文件（比如这里，只生成po，不生成controller和service以及dao）
+        templateConfig.setController("");
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        ///strategy.setSuperEntityClass("com.example.demo.entity");
+        strategy.setEntitySerialVersionUID(false);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        //strategy.setSuperControllerClass("com.example.demo.common.BaseController");
+        strategy.setInclude(tableName.split(","));
+        //strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix("t_");
+        // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
+        //strategy.setTablePrefix(new String[] { "tlog_", "tsys_" });// 此处可以修改为您的表前缀
+        //strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        // strategy.setInclude(new String[] { "user" }); // 需要生成的表
+        // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+        // 自定义实体父类
+        // strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
+        // 自定义实体，公共字段
+        // strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
+        // 自定义 mapper 父类
+        // strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
+        // 自定义 service 父类
+        // strategy.setSuperServiceClass("com.baomidou.demo.TestService");
+        // 自定义 service 实现类父类
+        // strategy.setSuperServiceImplClass("com.baomidou.demo.TestServiceImpl");
+        // 自定义 controller 父类
+        // strategy.setSuperControllerClass("com.baomidou.demo.TestController");
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // strategy.setEntityColumnConstant(true);
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        // strategy.setEntityBuilderModel(true);
+        mpg.setStrategy(strategy);
+        //mpg.setTemplateEngine(new VelocityTemplateEngine());
+        mpg.execute();
+    }
+}
